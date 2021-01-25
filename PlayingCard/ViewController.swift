e9c655e10c91fe30969aeaced2f95d84a013effe
Julia Stodolya <y.stodolya@gmail.com>
2021-01-25 19:40:42 +0300

    animation

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -8,44 +8,94 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.right, .left]
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    
     var deck = PlayingCardDeck()
+    
+    private var faceUpCardViews: [PlayingCardView] {
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+    }
+    
+    private var faceUpCardViewsMatch: Bool {
+        return faceUpCardViews.count == 2 && faceUpCardViews[0].rank == faceUpCardViews[1].rank && faceUpCardViews[0].suit == faceUpCardViews[1].suit
+    }
+    
+    @IBOutlet var cardViews: [PlayingCardView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var cards: [PlayingCard​] = []
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: Int.random(in: 0..<cards.count))
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
         
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print(card)
+    }
+    
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                UIView.transition(with: chosenCardView,
+                                  duration: 0.6,
+                                  options: [.transitionFlipFromLeft],
+                                  animations: {
+                                    chosenCardView.isFaceUp.toggle()
+                                  } ,
+                                  completion: { finished in
+                                    if self.faceUpCardViewsMatch {
+                                        UIViewPropertyAnimator.runningPropertyAnimator(
+                                            withDuration: 0.6,
+                                            delay: 0,
+                                            options: [],
+                                            animations: {
+                                                self.faceUpCardViews.forEach {
+                                                    $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                                                }
+                                            },
+                                            completion: { position in
+                                                UIViewPropertyAnimator.runningPropertyAnimator(
+                                                    withDuration: 0.75,
+                                                    delay: 0,
+                                                    options: [],
+                                                    animations: {
+                                                        self.faceUpCardViews.forEach {
+                                                            $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                                            $0.alpha = 0
+                                                        }
+                                                    },
+                                                    completion: { position in
+                                                        self.faceUpCardViews.forEach {
+                                                            $0.isHidden = true
+                                                            $0.alpha = 1
+                                                            $0.transform = .identity
+                                                        }
+                                                    }
+                                                )
+                                            }
+                                        )
+                                    } else if self.faceUpCardViews.count == 2 {
+                                        self.faceUpCardViews.forEach { cardView in
+                                            UIView.transition(with: cardView,
+                                                              duration: 0.6,
+                                                              options: [.transitionFlipFromLeft],
+                                                              animations: {
+                                                                cardView.isFaceUp = false
+                                                              }
+                                            )
+                                        }
+                                    }
+                                  }
+                )
             }
-        }
-    }
-
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended: playingCardView.isFaceUp.toggle()
         default: break
-        }
-        
-    }
-    
-    
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
         }
     }
     
 }
-
